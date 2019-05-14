049447c0a6753f653d758517d964ad22621e72fe
myungwoo.yun <myungwoo.yun@playd.com>
2019-05-14 13:45:35 +0900

    테스트

---
+++ webCrawling.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+req = requests.get('https://gausad.com/common/info')
+
+html = req.text
+
+soup = BeautifulSoup(html,'html.parser')
+gaus_title = soup.head.title
+gaus_img = soup.select('#contanier > div.GAUS_box > img')
+ttimg = gaus_img[0].get('alt')
+
+print(ttimg)
+
